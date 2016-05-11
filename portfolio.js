0e81d34a369507338af4a5cb4711e4a0e5bb4a94
jjschnei <jeffjschneider@gmail.com>
2016-05-10 20:45:57 -0700

    jQuery toggle for protfolio

--- portfolio.js
+++ portfolio.js
@@ -1,13 +1,36 @@
 $(document).ready(function(){
-	$('.entry').hover(function(){
 
 
+	$('img').hover(
 
+		function(){
 		$(this).animate({opacity: '.5'});
-	},
-	function() {
+	}, function() {
 		$(this).animate({opacity: '1'});
-	}
+	} 
 
 	);
+
+	
+
+	$('#img-1').hover(
+		function() {
+		$('.fig-1').toggle();
+	})
+
+	$('#img-2').hover(
+		function() {
+		$('.fig-2').toggle();
+	})
+
+	$('#img-3').hover(
+		function() {
+		$('.fig-3').toggle();
+	})
+
+	$('#img-4').hover(
+		function() {
+		$('.fig-4').toggle();
+	})
+
 });
