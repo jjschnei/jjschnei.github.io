8a720b8604113c05725c4f083f072cf93d26b909
jjschnei <jeffjschneider@gmail.com>
2016-05-19 14:39:02 -0700

    refactor jquery on porfolio page

--- portfolio.js
+++ portfolio.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
 
+// creats popdown windown on click
 	$('.popdown').popdown({width:800});
 
 
+// animates the portoflio image on mouse hover
 	$('img').hover(
-
 		function(){
 		$(this).animate({opacity: '.5'});
 	}, function() {
@@ -12,41 +13,11 @@ $(document).ready(function(){
 	});
 
 	
-
+// toggle the display of fig class on hover, which reveals the title of each project
 	$('figure').hover(
 		function(){
 			$(this).children('.fig').toggle();
 		})
 
-
-	$('#img-1').hover(
-		function() {
-		$('.fig-1').toggle();
-	})
-
-	$('#img-2').hover(
-		function() {
-		$('.fig-2').toggle();
-	})
-
-	$('#img-3').hover(
-		function() {
-		$('.fig-3').toggle();
-	})
-
-	$('#img-4').hover(
-		function() {
-		$('.fig-4').toggle();
-	})
-
-	$('#img-5').hover(
-		function() {
-		$('.fig-5').toggle();
-	})
-
-	$('#img-6').hover(
-		function() {
-		$('.fig-6').toggle();
-	})
 
 });
